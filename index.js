9e9116a3ba173ecc4d721b46f54fd4d911bc4320
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-04-10 21:34:01 -0400

    commit tree 88de1feaece936a704706a2f2beedf99f89493c4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jimmy",
+    streetAddress: "319 Sycamore Drive",
+};
+
+function updateEmployeeWithKeyAndValue(emplyee, key, value) {
+
+    const empl = { ...emplyee };
+    empl[key] = value;
+
+    return empl;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const empl = { ...employee };
+    delete empl[key]
+    return empl
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
